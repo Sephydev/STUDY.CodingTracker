809c290077d49a8b294624d634ce7147139ea79e
Sephydev <nicolas.bussiere@outlook.fr>
2026-04-26 15:36:35 +0200

    Add configuration

---
+++ STUDY.CodingTracker/Configuration/Configuration.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace STUDY.CodingTracker.Configuration;
+
+internal class Configuration
+{
+    public string ConnectionString { get; set; } = "";
+    public string DatabasePath { get; set; } = "";
+
+}
--- STUDY.CodingTracker/Program.cs
+++ STUDY.CodingTracker/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using STUDY.CodingTracker.Configuration;
+
+IConfiguration config = new ConfigurationBuilder()
+.SetBasePath(Directory.GetCurrentDirectory())
+.AddJsonFile("appsettings.json", optional: false,
+    reloadOnChange: true)
+.Build();
+
+var databaseSettings = config.GetSection("DatabaseSettings");
+
+Configuration myConfig = new Configuration();
+databaseSettings.Bind(myConfig);
+Console.WriteLine(myConfig.connectionString + myConfig.databasePath);
