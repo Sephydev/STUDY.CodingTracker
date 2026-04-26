3a57a03749589c7548640a08598b2687de086c92
Sephydev <nicolas.bussiere@outlook.fr>
2026-04-26 20:46:15 +0200

    Create db table

---
+++ STUDY.CodingTracker/Controllers/CodingSessionController.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace STUDY.CodingTracker.Controllers;
+
+internal class CodingSessionController
+{
+    private readonly IConfiguration _config;
+    private readonly string _connectionString;
+
+    public CodingSessionController(IConfiguration config)
+    {
+        _config = config;
+        var section = _config.GetSection("DataBaseSettings");
+        _connectionString = section["connectionString"] + section["databasePath"];
+
+        CreateTable();
+    }
+
+    // View
+
+    // Add
+
+    // Delete
+
+    // Update
+
+    private void CreateTable()
+    {
+        using var connection = new SqliteConnection(_connectionString);
+
+        connection.Open();
+
+        var codingSessionTable = connection.Execute(
+            @"CREATE TABLE IF NOT EXISTS codingSessions(
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            STARTTIME TEXT,
+            ENDTIME TEXT,
+            DURATION TEXT
+            )"
+        );
+    }
+}
--- STUDY.CodingTracker/Program.cs
+++ STUDY.CodingTracker/Program.cs
@@ -1,19 +1,12 @@
 ﻿using Microsoft.Extensions.Configuration;
-
+using STUDY.CodingTracker.Controllers;
 using STUDY.CodingTracker.Models;
-using STUDY.CodingTracker.Configuration;
 
 IConfiguration config = new ConfigurationBuilder()
 .SetBasePath(Directory.GetCurrentDirectory())
 .AddJsonFile("appsettings.json", optional: false,
     reloadOnChange: true)
 .Build();
-
-var databaseSettings = config.GetSection("DatabaseSettings");
-
-Configuration myConfig = new Configuration();
-databaseSettings.Bind(myConfig);
-Console.WriteLine($"Connection string: {myConfig.ConnectionString + myConfig.DatabasePath}");
 
 DateTime startTime;
 DateTime endTime;
@@ -22,3 +15,5 @@ DateTime.TryParse("01:12:27", out startTime);
 DateTime.TryParse("02:57:46", out endTime);
 CodingSessionModel codingSessionTest = new(startTime, endTime);
 Console.WriteLine($"Time test: {codingSessionTest.duration}");
+
+CodingSessionController codingSessionController = new(config);
