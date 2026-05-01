c38a08a6c2c2a93d5e11244aa5378660c0050c22
Sephydev <nicolas.bussiere@outlook.fr>
2026-05-01 15:49:24 +0200

    Add verification for date input

--- STUDY.CodingTracker/Helper/UserInput.cs
+++ STUDY.CodingTracker/Helper/UserInput.cs
@@ -6,8 +6,20 @@ internal static class UserInput
 {
     public static DateTime GetUserDateInput(string period)
     {
-        DateTime date = AnsiConsole.Ask<DateTime>($"Please enter the {period} date (format: HH:mm:ss):");
-        return date;
+        while (true)
+        {
+            Console.Clear();
+            string dateInput = AnsiConsole.Ask<string>($"Please enter the {period} date (format: HH:mm:ss):");
+
+            if (Verification.VerifyDate(dateInput).correct)
+            {
+                DateTime date = Verification.VerifyDate(dateInput).date;
+                return date;
+            }
+
+            AnsiConsole.WriteLine("The date you inputted were in a wrong format. Please try again! (Press Any Key to Continue).");
+            Console.ReadKey();
+        }
     }
 
     public static int GetUserIDInput(string operation)
---
+++ STUDY.CodingTracker/Helper/Verification.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace STUDY.CodingTracker.Helper;
+
+internal static class Verification
+{
+    public static (bool correct, DateTime date) VerifyDate(string dateInput)
+    {
+        bool correct = false;
+        DateTime date = new DateTime();
+
+        if (dateInput != null && DateTime.TryParseExact(dateInput, "HH:mm:ss", new CultureInfo("en-US"), DateTimeStyles.None, out date))
+        {
+            correct = true;
+        }
+
+        return (correct: correct, date: date);
+    }
+}
